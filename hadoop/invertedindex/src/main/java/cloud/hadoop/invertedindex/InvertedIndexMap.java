7295b8739c5ce04597c8edd7b899fad4d4e70ea8
MisaelVM <mvilchezmo@unsa.edu.pe>
2023-10-03 21:04:12 -0500

    Codigo mas limpio

--- hadoop/invertedindex/src/main/java/cloud/hadoop/invertedindex/InvertedIndexMap.java
+++ hadoop/invertedindex/src/main/java/cloud/hadoop/invertedindex/InvertedIndexMap.java
@@ -15,16 +15,12 @@ public class InvertedIndexMap extends Mapper<LongWritable, Text, Text, Text> {
     public void map(LongWritable key, Text value, Context context)
         throws IOException, InterruptedException {
 
-            // Split DocID and the actual text
             String DocId = value.toString().substring(0, value.toString().indexOf("\t"));
             String value_raw = value.toString().substring(value.toString().indexOf("\t") + 1);
 
-            // Reading input one line at a time and tokenizing by using space, "'", and "-" characters as tokenizers.
             StringTokenizer itr = new StringTokenizer(value_raw, " '-");
 
-            // Iterating through all the words available in that line and forming the key/value pair.
             while (itr.hasMoreTokens()) {
-                // Remove special characters
                 word.set(itr.nextToken().replaceAll("[^a-zA-Z]", "").toLowerCase());
                 if (word.toString() != "" && !word.toString().isEmpty()) {
                     context.write(word, new Text(DocId));
